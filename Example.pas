3c079cfaacf7010c63b12f00c92c66d82e0d5fe1
1Jae <wjkim@linkhub.co.kr>
2021-06-16 11:55:38 +0900

    added UseLocalTimeYN Config

--- Example.pas
+++ Example.pas
@@ -169,6 +169,10 @@ begin
         //인증토큰 IP제한기능 사용여부, true(권장)
         htTaxinvoiceService.IPRestrictOnOff := true;
 
+        //로컬시스템 시간 사용여부, true(사용), false(미사용) - 기본값
+        htTaxinvoiceService.UseLocalTimeYN := false;
+
+
         StringGrid1.Cells[0,0] := 'ntsconfirmNum';
         StringGrid1.Cells[1,0] := 'writeDate';
         StringGrid1.Cells[2,0] := 'issueDate';
