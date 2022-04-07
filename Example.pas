ce7bd4d31b235c84c846127e773e62ff274c3ad3
ProWON <pro_won45@naver.com>
2022-04-07 17:26:23 +0900

    remove 개인메일

--- Example.pas
+++ Example.pas
@@ -3,7 +3,7 @@
 { 팝빌 홈택스 전자세금계산서 매입/매출 API Delphi SDK Example
 {
 { - SDK 튜토리얼 : https://docs.popbill.com/httaxinvoice/tutorial/delphi }
-{ - 업데이트 일자 : 2022-01-10
+{ - 업데이트 일자 : 2022-04-07
 { - 연동 기술지원 연락처 : 1600-9854
 { - 연동 기술지원 이메일 : code@linkhubcorp.com
 {
